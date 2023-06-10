c32f3d8f885efc30b43a6345cb5a0f02b408a06d
Роман Левчук <rl@binotel.com>
2023-06-10 15:45:58 +0200

    update base app

---
+++ los-app/app/Http/Controllers/AvailabilityDataController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class AvailabilityDataController extends Controller
+{
+//    /**
+//     * @param Request $request
+//     * @return View
+//     */
+    public function getData(Request $request)
+    {
+        return 1;
+    }
+}
