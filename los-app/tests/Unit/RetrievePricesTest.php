d9cd69b4c48a1aa44a27698886a65bce8975e315
Роман Левчук <rl@binotel.com>
2023-06-17 23:10:23 +0200

    add test

--- los-app/tests/Unit/RetrievePricesTest.php
+++ los-app/tests/Unit/RetrievePricesTest.php
@@ -1,16 +1,14 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Unit;
 
+use Carbon\Carbon;
 use Tests\TestCase;
 use App\Models\PriceModel;
 use App\Repositories\PriceRepository;
 
 class RetrievePricesTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
     public function test_retrieve_prices_by_date(): void
     {
         $propertyId = '71438849-47cb-4b00-82de-34fff691f017';
@@ -29,5 +27,22 @@ class RetrievePricesTest extends TestCase
         $prices->each(function ($price) {
             $this->assertTrue($price instanceof PriceModel);
         });
+    }
+
+    public function test_price_by_date_exists(): void
+    {
+        $propertyId = '71438849-47cb-4b00-82de-34fff691f017';
+        $date = '2017-05-18';
+        $persons = 1;
+
+        $repository = new PriceRepository();
+        $priceExist = $repository->issetDatePrice(
+            $propertyId,
+            $date,
+            Carbon::parse($date)->dayOfWeek,
+            $persons
+        );
+
+        $this->assertTrue($priceExist);
     }
 }
